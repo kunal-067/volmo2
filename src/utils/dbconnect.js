2b452cffe13d1790ce1f620a93fc246cfd549ef3
itz-kunal <kunal.wd067@gmail.com>
2025-02-09 22:46:31 +0530

    lp

--- src/utils/dbconnect.js
+++ src/utils/dbconnect.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
-const dbUrl = process.env.DB_URI || 'mongodb+srv://valmoDriver1:DB%4078%2FConnector9%21%3F0@cluster0.zgcwg.mongodb.net/valmo2';
-
+const dbUrl = process.env.DB_URI || 'mongodb+srv://valmoDriver1:ClUsTeRAccEssOR@cluster0.zgcwg.mongodb.net/valmob';
 
 export async function connectDb() {
     try {
